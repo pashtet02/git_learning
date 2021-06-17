4e34756aa49079ebdef553342c262f046c00850d
Pavlo Khodachok <Pavlo_Khodachok@epam.com>
2021-06-17 13:13:21 +0300

    initial commit

---
+++ src/main/java/Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.printf("dfssdfs");
+        hello();
+    }
+
+    public static String hello() throws MyException{
+        return "hheee";
+    }
+}
